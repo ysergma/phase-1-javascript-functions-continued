23b0e9ae7cd2810f6e39e1b9ab93fbcf6126b5c8
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-07-24 13:16:01 +0100

    completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return "This Saturday, I want to "+activity+"!";
+
+}
+saturdayFun("play football");
+
+function mondayWork(activity="go to the office"){
+    return "This Monday, I will "+ activity +".";
+
+}
+mondayWork("play football");
+
+function wrapAdjective(para1="*"){
+    return function (para2=`special`) {
+            return `You are ${para1}${para2}${para1}!` ;
+    }
+}
+wrapAdjective("a hard worker");
+wrapAdjective("a hard worker");
